41e48bd574f492bf461a192bca4f6cad98a96dcb
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2025-04-23 15:37:26 +0100

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegisterClientComponent } from './register-client/register-client.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : '' , component : HomeComponent},
+    {path : 'register-client' , component : RegisterClientComponent},
+    {path : 'login' , component : LoginComponent },
+];
