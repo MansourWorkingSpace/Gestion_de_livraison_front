b6ac8971e5abc3536231567c980fcc562f85028e
SALAH_JNB <150282755+salah-jnb@users.noreply.github.com>
2025-05-10 13:53:48 +0100

    Update register-commercant.component.ts

--- src/app/register-commercant/register-commercant.component.ts
+++ src/app/register-commercant/register-commercant.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { InputFieldComponent } from "../components/input-field/input-field.component";
 import { SubmitButtonComponent } from "../components/submit-button/submit-button.component";
 
+
 @Component({
   selector: 'app-register-commercant',
   standalone: true,
