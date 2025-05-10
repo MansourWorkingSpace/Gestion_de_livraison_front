2453b8a64f2e7a53c86747503a7d9a402a4ba888
SALAH_JNB <150282755+salah-jnb@users.noreply.github.com>
2025-05-10 13:53:13 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import { RegisterLivreurComponent } from './register-livreur/register-livreur.component';
 import { RegisterCommercantComponent } from './register-commercant/register-commercant.component';
+import { QrScannerComponent } from './components/qr-scanner/qr-scanner.component';
 
 export const routes: Routes = [
     {path : '' , component : HomeComponent},
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path : 'login' , component : LoginComponent },
     {path : 'dashbord' , component : DashbordComponent},
     {path : 'register-livreur' , component : RegisterLivreurComponent},
-    {path : 'register-commercant' , component : RegisterCommercantComponent}
+    {path : 'register-commercant' , component : RegisterCommercantComponent},
+    {path : 'qr-scanner' , component : QrScannerComponent}
 ];
