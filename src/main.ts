2b0dd4ebd100cdef690e4e59e43aa4e563dcae49
SALAH_JNB <150282755+salah-jnb@users.noreply.github.com>
2025-05-10 13:53:57 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes'; // Ensure routes are defined
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(), // Provide HttpClient globally
     provideRouter(routes) // Provide routing if your app uses routes
+    , provideAnimations() // Provide animations if your app uses them
+    ,provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ]
 }).catch(err => console.error(err));
