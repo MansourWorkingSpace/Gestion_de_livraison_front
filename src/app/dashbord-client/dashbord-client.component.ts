698bb3c47503c06b685645e7df2cc5299553e3ee
MansourWorkingSpace <mansour.tech.contact@gmail.com>
2025-05-12 22:49:37 +0100

    dashboard Client

---
+++ src/app/dashbord-client/dashbord-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashbord-client.component.html',
+  styleUrl: './dashbord-client.component.css'
+})
+export class DashbordClientComponent {
+
+}
